fb91e33ce90cfde67dd85526bb7a01e77c575777
peter barraud <gapeterb@gmail.com>
2024-11-15 19:52:04 +0530

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,142 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// // Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
+
+
+// import Alert from 'bootstrap/js/dist/alert'
+
+
+// // or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'
+const InfoMessageType = {
+  ERR: 1,
+  WARN: 2,
+  INFO: Symbol("green")
+};
+Object.freeze(InfoMessageType);
+
+let balanceData = [];
+
+let restURL = 'http://localhost:8089/services/rest.api.php';
+
+
+function infoBoxUpdate(infoMsg, infoMsgType){
+  let classList = document.querySelector("#info-box").classList
+  classList.remove(...classList);
+  classList.add("alert");
+  document.querySelector("#info-box").innerHTML = infoMsg;
+  if (infoMsgType === InfoMessageType.ERR){
+    classList.add("alert-danger");
+  } else if (infoMsgType === InfoMessageType.WARN){
+    classList.add("alert-warning");
+  } else if (infoMsgType === InfoMessageType.INFO){
+    classList.add("alert-primary");
+  }
+}
+
+function getallbalances(){
+  fetch(`${restURL}/getallbalances/`, {
+    method: "GET"
+}).then(response => response.json())
+.then(data => {
+  let balanceListGroup = document.querySelector("#balance-list-group");
+  balanceListGroup.innerHTML = null;
+  data.forEach(balanceObject => {
+    balanceData[balanceObject.id] = {value:balanceObject.value, datetime:new Date(balanceObject.balancetime_ts.replace(" ","T"))}
+    let listGroupItem = document.createElement('li');
+    listGroupItem.classList.add("list-group-item")
+    listGroupItem.appendChild(
+      Object.assign(
+        document.createElement("input"),
+        {
+          class: "form-check-input me-1",
+          type: "checkbox",
+          name:'list-group-item',
+          id:balanceObject.id
+        }
+      )
+    );
+
+    listGroupItem.appendChild(
+      Object.assign(
+        document.createElement("label"),
+        {
+          class: "form-check-label",
+          innerHTML:`&#8377;${balanceObject.value}; ${balanceObject.balancetime_ts}`,
+          style:"margin-left:5px"
+        }
+      )
+    );
+
+    balanceListGroup.appendChild(listGroupItem);
+
+  });
+})
+.catch((err) => {
+  console.log(err);
+})
+}
+
+
+
+window.onload = function(){
+  if(window.location.href.includes("localhost")){
+    restURL = 'http://localhost:8089/services/rest.api.php'
+  } else {
+    restURL = 'https://meter.peterb.in/services/rest.api.php/'
+  }
+
+  
+  getallbalances();
+};
+
+
+document.querySelector("#save").addEventListener("click", function (e) {
+  if (document.querySelector("#meter-balance").value && parseInt(document.querySelector("#meter-balance").value) >= 0){
+    fetch(`${restURL}/savebalance/`, {
+      method: "POST",
+      body: JSON.stringify({
+        balance: {
+          value: document.querySelector("#meter-balance").value
+        }
+     })})
+    .then(response => response.json())
+    .then(data => {
+      infoBoxUpdate(`New balance entry <b>${data.value}</b>`,InfoMessageType.INFO);
+      getallbalances();
+    })
+    .catch((err) => {
+      console.log(err);
+    })} else {
+      infoBoxUpdate("Enter some balance greater than zero",InfoMessageType.ERR);
+    }
+  
+});
+document.querySelectorAll('[time-period]').forEach(button => 
+  button.addEventListener('click', () => {
+    if (document.querySelectorAll("[name='list-group-item']:checked").length == 2){
+      // let date2 = document.querySelectorAll("[name='list-group-item']:checked")[1].datetime;
+      let date1 = balanceData[document.querySelectorAll("[name='list-group-item']:checked")[0].id].datetime;
+      let date2 = balanceData[document.querySelectorAll("[name='list-group-item']:checked")[1].id].datetime;
+      let balance1 = balanceData[document.querySelectorAll("[name='list-group-item']:checked")[0].id].value;
+      let balance2 = balanceData[document.querySelectorAll("[name='list-group-item']:checked")[1].id].value;
+      let ratepermillisecond = Math.abs(balance1 - balance2)/Math.abs((date1 - date2));
+      let outputrate = ratepermillisecond*1000*3600*24;
+      if (button.attributes['time-period'].value == "month"){
+        outputrate = outputrate*(365.25/12);
+      }
+      infoBoxUpdate(`Rate per day: &#8377;${Math.round(outputrate)}`, InfoMessageType.INFO)
+    } else {
+      infoBoxUpdate("You need to select <b>ONLY</b> two dates", InfoMessageType.ERR)
+    }
+      
+  })
+);
+
+
+
+
+
+
